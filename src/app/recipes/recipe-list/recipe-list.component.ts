8c1558b823c86ca0cf971e0e48c30c09e5d7b3bd
krishnadutt <krishna.suddhapally@gmail.com>
2022-04-07 23:39:49 +0530

    recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+ recipes : Recipe[] = [
+   new Recipe('Pulao','Veggie special' ,'https://upload.wikimedia.org/wikipedia/commons/8/86/Homemade_Veg_Pulao.jpg'),
+   new Recipe('Veg Biryani','Veggie special Biryani' ,'https://pixahive.com/wp-content/uploads/2020/10/Spicy-veg-biryani-143389-pixahive.jpg'),
+   new Recipe('Chapati','Veg' ,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5uf6EaNSqf45NkUe2-Q2m1pd9rYbptJhNIA&usqp=CAU')
+ ];
   constructor() { }
 
   ngOnInit(): void {
