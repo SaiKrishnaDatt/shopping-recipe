e6a13a93583d738e088c8d4434b1611f18d07177
krishnadutt <krishna.suddhapally@gmail.com>
2022-04-26 18:46:53 +0530

    configuring route parameter

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.services';
 
@@ -8,10 +9,21 @@ import { RecipeService } from '../recipe.services';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-@Input() recipe : Recipe;
-  constructor(private recipeService :RecipeService) { }
+recipe : Recipe;
+id : number;
+  constructor(private recipeService :RecipeService,private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
+    //-> below line will work only one time loading
+   // const id = this.activatedRoute.snapshot.params['id']
+
+   //but need more than one time load
+   //+ sign here is type casting to number from integer
+   this.activatedRoute.params.subscribe((params :Params)=>
+    {this.id = +params['id'];
+    this.recipe = this.recipeService.getRecipesById(this.id);
+  }
+    );
   }
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.services';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,13 +8,10 @@ import { RecipeService } from '../../recipe.services';
 })
 export class RecipeItemComponent implements OnInit {
  @Input() recipe: Recipe;
-  constructor(private recipeService : RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe);
 
-  }
 
 }
